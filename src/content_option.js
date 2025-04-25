783d5c4e985957e68e984de2813a1935752c8896
noorophobia <noorf3531@gmail.com>
2025-04-26 00:42:49 +0500

    d

---
+++ src/content_option.js
@@ -0,0 +1,132 @@
+ 
+ const logotext = "Name ";
+const meta = {
+    title: "Name ",
+    description: "I’m Name  Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
+};
+
+const introdata = {
+    title: "I’m Name ",
+    animated: {
+        first: "I love coding",
+        second: "I develop web apps",
+        third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. "
+    ,     your_img_url: "ADD IMAGE URL HERE "};
+
+const dataabout = {
+    title: " about my self",
+    aboutme: " I am a passionate and results-driven developer with experience in Android application development using Java and XML, as well as full-stack development with the MERN stack. I thrive on building user-centric applications that solve real-world problems, combining intuitive designs with robust functionality."
+ };
+ 
+const skills = [{
+        name: "Java",
+        value: 0,
+    },
+    {
+        name: "C++",
+        value: 0,
+    },
+   
+    
+];
+
+const services = [{
+         
+            title: "UI & UX Design",
+            description: "Creating intuitive and visually appealing designs that enhance user experience and engagement."
+        },
+        {
+            title: "Android Apps",
+            description: "Developing high-performance Android applications with robust functionality and seamless UI."
+        },
+        {
+            title: "Web Development",
+            description: "Building responsive and scalable websites using the MERN."
+        },
+        {
+            title: "Backend Development",
+            description: "Designing and implementing secure and efficient server-side architectures for smooth data handling."
+        },
+         
+    
+];
+
+const dataportfolio = [
+    {
+        img: "ADD IMAGE URL / IMAGE PATH", // thumbnail or a screenshot
+        description: "ADD DESC",
+        link: "ADD VIDEO LINK", // link to the video
+      },
+      {
+        img: "ADD IMAGE URL / IMAGE PATH", // thumbnail or a screenshot
+        description: "ADD DESC",
+        link: "ADD VIDEO LINK", // link to the video
+      }
+      
+     
+];
+const dip = [
+    {
+        
+            img: "ADD IMAGE URL / IMAGE PATH", // thumbnail or a screenshot
+            description: "ADD DESC",
+            link: "ADD VIDEO LINK", // link to the video
+             },];
+const figma = [
+    {
+        img: "ADD IMAGE URL / IMAGE PATH", // thumbnail or a screenshot
+        description: "ADD DESC",
+        link: "ADD VIDEO LINK", // link to the video
+      },
+      {
+        img: "ADD IMAGE URL / IMAGE PATH", // thumbnail or a screenshot
+        description: "ADD DESC",
+        link: "ADD VIDEO LINK", // link to the video
+      }];
+ const contactConfig = {
+    YOUR_EMAIL: "ADD EMAIL ",
+    YOUR_FONE: "ADD NUMBER",
+    description: "You can either email me or whatsapp me for queries or job offers"
+     // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    
+};
+
+const worktimeline = [{
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+},
+{
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+},
+{
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+},
+];
+ 
+const socialprofils = {
+    github: "https://github.com/",
+     linkedin: "https://www.linkedin.com/in/",
+ };
+ export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+    figma,
+    dip,
+};
+   
